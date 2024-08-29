e4d72a5c7386a60f7788f6588d3aa430d7755ce8
23saraw <sw122@rice.edu>
2024-08-29 14:18:49 -0500

    navbar apply button updated

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -29,7 +29,7 @@ const NavBar: React.FC = () => {
           <span className="absolute left-0 bottom-0 w-full h-0.5 bg-white scale-x-0 group-hover:scale-x-100 transform transition-transform origin-left"></span>
         </li>
         <li className="relative group">
-          <a href="https://docs.google.com/forms/d/e/1FAIpQLSd1gADwnj7PzFoeItHl3euH8QRYT42ywjV5bWp0hLXvaCtIww/viewform?usp=sf_link" target="_blank" rel="noreferrer" className="text-white">Apply</a>
+          <a href="https://linktr.ee/hackrice14" target="_blank" rel="noreferrer" className="text-white">Apply</a>
           <span className="absolute left-0 bottom-0 w-full h-0.5 bg-white scale-x-0 group-hover:scale-x-100 transform transition-transform origin-left"></span>
         </li>
       </ul>
