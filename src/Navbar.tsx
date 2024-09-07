b314b49b949112cb2ad72b6daa0a20f5478949af
23saraw <sw122@rice.edu>
2024-09-07 16:44:53 -0500

    navbar

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -11,12 +11,12 @@ const NavBar: React.FC = () => {
     <nav className="flex justify-between items-center bg-black h-16 w-screen p-4 fixed top-0 left-0 z-10">
       <div className="text-white font-semibold">
         <button
-          className="block md:hidden text-white text-2xl sm:text-3xl focus:outline-none"
+          className="block md:hidden text-white text-2xl sm:text-3xl focus:outline-none z-20 relative"
           onClick={toggleDropdown}
         >
           ☰
         </button>
-        <ul className={`sm:flex ${isOpen ? "block" : "hidden"} md:block h-auto space-y-4 md:space-y-0 md:space-x-8 ml-4 mt-48 md:mt-0`}>
+        <ul className={`md:flex ${isOpen ? "block" : "hidden"} md:block h-auto space-y-4 md:space-y-0 md:space-x-8 ml-5 mt-4 md:mt-0 z-10 bg-black md:bg-transparent w-auto md:w-auto absolute md:static left-0`}>
           <li className="relative group">
             <a href="#home" className="text-white hover:text-gray-200">Home</a>
             <span className="absolute left-0 bottom-0 w-full h-0.5 bg-white scale-x-0 group-hover:scale-x-100 transform transition-transform origin-left"></span>
@@ -47,7 +47,6 @@ const NavBar: React.FC = () => {
           </li>
         </ul>
       </div>
-    
     </nav>
   );
 };
