3d0fc70c655712d94deb6b3c40b137eafece6b96
Faith <faithzhng@gmail.com>
2025-05-27 21:40:44 -0700

    more changes to UI

--- src/App.tsx
+++ src/App.tsx
@@ -91,11 +91,13 @@ export default function App(){
     return () => clearInterval(interval);
   }, [currentTextIndex, texts, speed]);
 
+  
   return (
     <div className="flex flex-col justify-center items-center h-auto w-screen m-auto left-0 right-0 overflow-x-hidden">
-      <a id="mlh-trust-badge" className="absolute w-20 h-auto z-10 top-0 md:top-0 right-5 md:w-24 lg:w-36" href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2025-season&utm_content=white" target="_blank" rel="noopener noreferrer">
-        <img 
-          src="https://s3.amazonaws.com/logged-assets/trust-badge/2025/mlh-trust-badge-2025-white.svg" 
+
+      <a id="mlh-trust-badge" className="absolute w-20 h-auto z-10 top-0 md:top-0 right-5 md:w-24 lg:w-36" 
+      href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2026-season&utm_content=white" target="_blank" rel="noopener noreferrer">
+        <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2026/mlh-trust-badge-2026-white.svg" 
           alt="Major League Hacking 2025 Hackathon Season">
         </img>
       </a>
@@ -112,21 +114,21 @@ export default function App(){
       
   {/* -------------- ABOUT --------------- */}
       <div id="about" className="flex flex-col items-center mx-auto left-0 right-0 mb-32 md:mb-44 lg:mb-20 scroll-mt-24">
-        <h1 className=" text-white font-extrabold text-3xl lg:text-4xl text-center">About</h1>
-        <h2 className="text-blue-800 text-opacity-90 font-extrabold text-xl text-center mt-3 md:ml-0 md:mr-0 ml-10 mr-10">
+        <h1 className=" text-white font-extrabold text-3xl lg:text-4xl text-center">about</h1>
+        <h2 className="text-white-800 text-opacity-90 font-extrabold text-xl text-center mt-3 md:ml-0 md:mr-0 ml-10 mr-10">
         {displayedTexts[0]}
         </h2>
           <div className="flex flex-col items-center lg:grid lg:grid-cols-2 mt-12 lg:mt-0 lg:ml-12 xl:ml-28">
           {/* -------------- INFORMATION --------------- */}
             <div className="relative flex flex-col items-center justify-center h-100 w-81 sm:h-83 sm:w-96 lg:h-96">
-              <div className="absolute outline bg-blue-800 bg-opacity-20 outline-blue-800 rounded-xl ml-4 mt-8 p-8 h-99 w-11/12 sm:h-84 md:1/2 lg:h-3/4 mx-auto">
+              <div className="absolute outline bg-white-800 bg-opacity-20 outline-white-800 rounded-xl ml-4 mt-8 p-8 h-99 w-11/12 sm:h-84 md:1/2 lg:h-3/4 mx-auto">
                 <h1 className="hidden font-medium text-lg text-center">
                   HackRice is Rice University's annual hackathon. It brings together over 400 of the brightest minds in the country to develop & showcase their creative and entrepreneurial talents. 
                   Participants form teams of up to 4 to work on a project — typically software, and sometimes hardware — in an environment designed for collaboration. Teams showcase projects after 36 hours of hacking. 
                   <br/> <br/> This is an excellent opportunity for students to build a network of creators, receive internship & job offers from world-class sponsors, and learn modern tools & systems.
                 </h1>
               </div>
-              <div className="absolute outline bg-blue-800 bg-opacity-20 outline-blue-800 rounded-xl mr-4 p-8 h-99 w-11/12 sm:h-84 md:1/2 lg:h-3/4 mx-auto">
+              <div className="absolute outline bg-opacity-20 outline-white-800 rounded-xl mr-4 p-8 h-99 w-11/12 sm:h-84 md:1/2 lg:h-3/4 mx-auto">
                 <h1 className="text-white font-medium text-lg text-center">
                   HackRice is Rice University's annual hackathon. It brings together over 400 of the brightest minds in the country to develop & showcase their creative and entrepreneurial talents. 
                   Participants form teams of up to 4 to work on a project — typically software, and sometimes hardware — in an environment designed for collaboration. Teams showcase projects after 36 hours of hacking. 
@@ -137,15 +139,15 @@ export default function App(){
         {/* -------------- APPLY --------------- */}
             <div className="relative flex flex-col items-center lg:ml-20 mt-10 sm:mt-20 lg:mt-0 h-81 w-81 sm:h-80 sm:w-84 md:w-96 lg:w-81">
               <div className="absolute outline outline-blue-800 rounded-xl mr-4 p-8 h-80 w-11/12 md:h--3/4 md:w-80">
-                <h1 className="invisible text-center text-white text-xl font-medium sm:mb-4 sm:mt-0 md:mt-4 lg:block">September 20, 2024 - September 22, 2024</h1> 
+                <h1 className="invisible text-center text-white text-xl font-medium sm:mb-4 sm:mt-0 md:mt-4 lg:block">September 19, 2025 - September 21, 2025</h1> 
                 <h2 className="invisible text-center text-white text-xl font-medium max-w-64 sm:max-w-full lg:block">Rice Memorial Center @ Rice University</h2>
               </div>
               <div className = "absolute bg-gradient-to-b from-blue-900/70 via-blue-800/70 to-orange-700/70 bg-opacity-15 rounded-xl ml-4 md:ml-8 mt-4 p-8 h-80 w-11/12 sm:h-81 md:h-81 md:w-81">
-                <h1 className = "invisible text-center text-white text-xl font-medium sm:mb-4 mb-3 md:mt-4">September 20, 2024 - September 22, 2024</h1> 
+                <h1 className = "invisible text-center text-white text-xl font-medium sm:mb-4 mb-3 md:mt-4">September 19, 2025 - September 21, 2025</h1> 
                 <h2 className = "invisible text-center text-white m-auto text-xl font-medium max-w-64 sm:max-w-full">Rice Memorial Center @ Rice University</h2>
               </div>
               <div className = "absolute flex flex-col items-center justify-center bg-black bg-opacity-75 rounded-xl ml-5 md:ml-8 mt-8 md:mt-8 p-3 h-72 w-5/6 sm:h-73 sm:w-82 md:h-73 md:w-73">
-                <h1 className = "text-center text-white text-xl sm:text-2xl font-medium">September 20-22, 2024</h1> 
+                <h1 className = "text-center text-white text-xl sm:text-2xl font-medium">September 19-21, 2025</h1> 
                 <h2 className = "text-center text-white text-xl sm:text-2xl mt-6 font-medium">Rice Memorial Center <br/> @ Rice University</h2>
                 <a className="flex items-center justify-center hover:outline-orange-600/40 bg-blue-800 hover:bg-orange-600/15 bg-opacity-20 outline outline-blue-800 rounded-xl text-center text-white text-xl sm:text-2xl font-bold mt-6 h-14 w-36 sm:w-40" href="https://linktr.ee/hackrice14" target="_blank" rel="noreferrer"> apply now </a> 
               </div>
